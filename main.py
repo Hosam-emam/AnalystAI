c86974fdd2c90f7cf05eaa7f4fd3cf29bc3e6bd4
Hosam_Hazem <hosamzoma77@gmail.com>
2026-02-08 23:51:18 +0200

    Use main.py as an entry point for the

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from data-analysis-agent!")
+import uvicorn
+from src.routers import base_router
+from fastapi import FastAPI
 
+def main():
+    app = FastAPI()
+
+    app.include_router(base_router)
+
+    uvicorn.run(
+        app=app,
+        host="127.0.0.1",
+        port=5000
+    )
 
 if __name__ == "__main__":
     main()
